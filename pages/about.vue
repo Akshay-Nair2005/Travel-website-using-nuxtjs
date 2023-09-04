6fb8bd49efff3362562b0db2b61cf72bac9f1a1a
Akshay-Nair2005 <115722931+Akshay-Nair2005@users.noreply.github.com>
2023-09-04 19:04:10 +0530

    second commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,56 +1,61 @@
 <template>
-    <header>
-        <nav class="navbar">
-            <ul>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/destination">Destination</a></li>
-                <li><a href="/review">Review</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    </header>
-    <div class="about-us-page">
-        <section class="about-content">
-            <h1>About Us</h1>
-            <p>Welcome to Travelopedia, your trusted partner for unforgettable travel experiences.</p>
-            <p>We are a passionate team of travel enthusiasts dedicated to creating memorable journeys that cater to all
-                kinds of travelers. Whether you're an adventure seeker, a culture explorer, or a relaxation lover, we have
-                something special for everyone.</p>
-            <p>Our mission is to provide you with seamless travel experiences, from the moment you dream about your trip to
-                the moment you return home with a heart full of memories. We handpick the best destinations, accommodations,
-                and activities to ensure you have a truly immersive and enjoyable experience.</p>
-            <p>Join us on a journey that goes beyond the ordinary, and let's explore the world together.</p>
-        </section>
-        <h2>Meet Our Team</h2>
-        <br><br>
-        <section class="team-members">
-            <div class="team-member">
-                <img src="./static/c_c2.jpg" alt="Team Member 1" />
-                <h3>Akshay Nair</h3>
-                <p>Founder & CEO</p>
-            </div>
-            <div class="team-member">
-                <img src="./static/c_c2.jpg" alt="Team Member 2" />
-                <h3>Amir Chaus</h3>
-                <p>Travel Specialist</p>
-            </div>
-            <div class="team-member">
-                <img src="./static/c_c2.jpg" alt="Team Member 2" />
-                <h3>Aaditi Bhosale</h3>
-                <p>Travel Specialist</p>
-            </div>
-            <div class="team-member">
-                <img src="./static/c_c2.jpg" alt="Team Member 2" />
-                <h3>Aravind Gajula</h3>
-                <p>Travel Specialist</p>
-            </div>
-            <div class="team-member">
-                <img src="./static/c_c2.jpg" alt="Team Member 2" />
-                <h3>Sanika Warange</h3>
-                <p>Travel Specialist</p>
-            </div>
-            <!-- Add more team members as needed -->
-        </section>
+    <div class="body">
+        <header>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="/destination">Destination</a></li>
+                    <li><a href="/review">Review</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="about-us-page">
+            <section class="about-content">
+                <h1>About Us</h1>
+                <p>Welcome to Travelopedia, your trusted partner for unforgettable travel experiences.</p>
+                <p>We are a passionate team of travel enthusiasts dedicated to creating memorable journeys that cater to all
+                    kinds of travelers. Whether you're an adventure seeker, a culture explorer, or a relaxation lover, we
+                    have
+                    something special for everyone.</p>
+                <p>Our mission is to provide you with seamless travel experiences, from the moment you dream about your trip
+                    to
+                    the moment you return home with a heart full of memories. We handpick the best destinations,
+                    accommodations,
+                    and activities to ensure you have a truly immersive and enjoyable experience.</p>
+                <p>Join us on a journey that goes beyond the ordinary, and let's explore the world together.</p>
+            </section>
+            <h2>Meet Our Team</h2>
+            <br><br>
+            <section class="team-members">
+                <div class="team-member">
+                    <img src="./static/c_c2.jpg" alt="Team Member 1" />
+                    <h3>Akshay Nair</h3>
+                    <p>Founder & CEO</p>
+                </div>
+                <div class="team-member">
+                    <img src="./static/c_c2.jpg" alt="Team Member 2" />
+                    <h3>Amir Chaus</h3>
+                    <p>Travel Specialist</p>
+                </div>
+                <div class="team-member">
+                    <img src="./static/c_c2.jpg" alt="Team Member 2" />
+                    <h3>Aaditi Bhosale</h3>
+                    <p>Travel Specialist</p>
+                </div>
+                <div class="team-member">
+                    <img src="./static/c_c2.jpg" alt="Team Member 2" />
+                    <h3>Aravind Gajula</h3>
+                    <p>Travel Specialist</p>
+                </div>
+                <div class="team-member">
+                    <img src="./static/c_c2.jpg" alt="Team Member 2" />
+                    <h3>Sanika Warange</h3>
+                    <p>Travel Specialist</p>
+                </div>
+                <!-- Add more team members as needed -->
+            </section>
+        </div>
     </div>
 </template>
   
@@ -118,8 +123,8 @@ p {
 
 .body {
     font-family: Arial, sans-serif;
-    background: url('~/staticc/beach1.jpg') no-repeat center center/cover;
-    /* background-color: white; */
+    /* background: url('~/staticc/beach1.jpg') no-repeat center center/cover; */
+    background-color: white;
     height: 100%;
     /* ... (other existing styles) */
     /* Remove any margin or padding from the .body element */
--- pages/home.vue
+++ pages/home.vue
@@ -1,364 +1,241 @@
-
 <template>
-    <div class="body">
-        <nav id="navbar">
-            <div id="logo">
-                <img src="./static/logo.jpg" alt="logo" />
-                <h1 class="Akki">Travelopedia</h1>
+    <div>
+        <header class="header">
+            <nav class="navcontainer">
+                <a href="#"><span>T</span>ravelopedia</a>
+            </nav>
+            <nav class="navbar">
+                <ul>
+                    <li><a href="#home">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="whole">
+            <div class="container">
+                <h1>Welcome to Travelopedia</h1><br>
+                <h3>Explore the World</h3><br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel perferendis</p><br>
+                <a href="#">
+                    <p class="explore">Start Your Journey</p>
+                </a><br>
             </div>
 
-            <ul class="unordy">
-                <!-- <li class="item"><a href="index.vue">Destination</a></li>
-                <li class="item"><a href="#">About us</a></li>
-                <li class="item"><a href="#">Help</a></li>
-                <li class="item"><a href="#">Contact</a></li> -->
-                <li class="item"><nuxt-link to="/destination">Destination</nuxt-link></li>
-                <li class="item"><nuxt-link to="/about">About Us</nuxt-link></li>
-                <li class="item"><nuxt-link to="/review">Review</nuxt-link></li>
-                <li class="item"><nuxt-link to="/contact">Contact</nuxt-link></li>
-            </ul>
-        </nav>
-
-        <section id="home">
-            <h1 class="head">Welcome to Kingdom of Heaven</h1>
-            <p>Explore your dreams with us.... within and beyond the borders </p>
-            <p>"Where Dreams come alive..."</p>
-            <p>- Akshay Nair</p>
-            <!-- <a href="index.vue" class="a" target="_blank">Explore</a> -->
-            <nuxt-link to="/destination" class="a">Explore</nuxt-link>
-        </section>
-
-        <footer class="foot">
-            <div id="footer-top" class="social-footer">
-                <div class="container">
-                    <div class="region region-footer-top">
-                        <div id="block-block-1" class="block block-block">
-                            <div class="content">
-                                <div class="col-md-12 sf-icons">
-                                    <a href="#" target="_blank"> <span class="icon">
-                                            <font-awesome-icon :icon="['fab', 'youtube']" /></span> </a>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a href="#" target="_blank"><span class="icon"><font-awesome-icon
-                                                :icon="['fab', 'instagram']" /></span></a>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a href="#" target="_blank"><span class="icon"><font-awesome-icon
-                                                :icon="['fab', 'twitter']" /></span></a>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a href="#" target="_blank"><span class="icon"><font-awesome-icon
-                                                :icon="['fab', 'telegram']" /></span></a>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;
-                                    <a href="#" target="_blank"><span class="icon"><font-awesome-icon
-                                                :icon="['fab', 'facebook']" /></span></a>
-                                    &nbsp;&nbsp;&nbsp;&nbsp;
-                                </div>
-                                <div class="row">
-                                    <div class="col col-sm-12">
-                                        <div class="foot">
-                                            <p>&copy; 2012 Tourism Company. All rights reserved.</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <div class="carousel-container">
+                <div class="carousel">
+                    <img src="./static/nature5.jpg" alt="Image 1">
+                    <img src="./static/nature6.jpg" alt="Image 2">
+                    <img src="./static/nature3.jpg" alt="Image 3">
+                    <img src="./static/nature4.jpg" alt="Image 4">
+                    <img src="./static/nature7.jpg" alt="Image 5">
+                    <img src="./static/nature8.jpg" alt="Image 6">
                 </div>
             </div>
-        </footer>
+        </div>
     </div>
 </template>
-
-    
-<style scoped>
-/* Your CSS styles here */
-/* CSS reset and other styles... */
-/* css reset */
-
+  
+<style>
+/* Your CSS styles go here */
 * {
     margin: 0;
     padding: 0;
-    /* background-color: black; */
+    box-sizing: border-box;
 }
 
-.body {
-    /* border: 2px solid red; */
-    padding: 0;
+html {
+    scroll-behavior: smooth;
 }
 
-:root {
-    --navbar-height: 59px;
-
-
+body {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1em;
+    color: rgb(165, 165, 165);
+    background: url('~/staticc/nature4.jpg') no-repeat center center/cover;
+    /* Replace 'background.jpg' with your actual background image path */
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    /* opacity: 0.85; */
 }
 
-
-/* Navigation Bar */
-#navbar {
+.header {
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    /* justify-content: center; */
+    padding: 20px;
 }
 
-/* Nav: Logo and image */
-
-#logo {
-    margin: 34px 25px;
-
-}
-
-#logo img {
-
-    margin: -22px 5px;
-    height: 110px;
-    width: 150px;
-
-}
-
-/* ChefYChef */
-.Akki {
-    font-size: 2.0rem;
+.navcontainer {
+    font-size: 1.5em;
     color: white;
-    display: flex;
-    position: absolute;
-    font-family: 'Poppins', cursive;
-    /* padding-left: 10px; */
-    text-align: left;
-    /* align-items: left; */
-    /* justify-content: flex-start; */
-    margin: 5px 5px;
-
-}
-
-/* .Akki h1::before {
-        z-index: -1;
-    } */
-
-/* Nav: Listing Items */
-
-#navbar {
-    position: relative;
-    /* width: 100%; */
-}
-
-#navbar::before {
-    content: "";
-    background-color: black;
-    position: absolute;
-    height: 120%;
-    width: 100%;
-    z-index: -1;
-    opacity: 1;
-}
-
-#navbar ul {
-    display: flex;
-
-}
-
-
-#navbar ul li {
-    /* color: white;     */
-    list-style: none;
-    font-size: 1.2rem;
-}
-
-#navbar ul li a {
-    display: block;
-    padding: 5px 40px;
-    border-radius: 7px;
     text-decoration: none;
-    color: azure;
-    font-family: 'Bree Serif', serif;
-    font-size: 1.5rem;
+    margin-top: 3.5%;
+    margin-left: 5%;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
 }
 
-#navbar ul li a:hover {
-    color: azure;
-    font-size: 1.7rem;
-    /* background-color: white; */
-    /* text-decoration: underline; */
+.navcontainer a {
+    font-size: 1.3em;
+    color: white;
+    text-decoration: none;
+    margin-left: 3%;
 }
 
-/* home section */
+.navcontainer span {
+    font-size: 1.5em;
+    color: orange;
 
-#home {
+}
+
+
+
+.navbar {
+    /* border: 2px solid white; */
+    margin-top: 3.5%;
+    margin-right: 10%;
+}
+
+.navbar ul {
+    list-style: none;
     display: flex;
-    flex-direction: column;
-    padding: 3px 200px;
-    margin-top: 4%;
+}
+
+.navbar li {
+    margin-right: 15%;
+    /* border: 2px solid red; */
+    /* padding-right: 3%; */
+}
+
+.navbar a {
+    text-decoration: none;
+    color: white;
+    /* transition: all .5s; */
+}
+
+.navbar a:hover {
+    text-decoration: underline;
+    /* color: white; */
+}
+
+.container {
+    text-align: left;
+    padding: 7%;
+    margin-top: 1.6%;
+    color: white;
+}
+
+.container a {
+    text-decoration: none;
+}
+
+.explore {
+    margin-top: 1.5%;
+    font-size: 1.2em;
+    background-color: transperant;
+    border: 2px solid orange;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+    width: 40%;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
+
+h1,
+h3,
+p {
+    padding-top: 0.8%;
+}
+
+.dest {
+    display: flex;
+    flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    /* position: relative; */
-    height: 290px;
-
 }
 
-#home::before {
-    content: "";
-    background: url('~/staticc/Bgimage_home.webp') no-repeat center center/cover;
-    position: absolute;
-    /* border: 2px solid red; */
-    /* height: 80%; */
-    height: 110%;
-    width: 100%;
-    z-index: -1;
-    opacity: 0.89;
-}
-
-#home h1 {
-    color: white;
-    text-align: center;
-    font-family: 'Bree Serif', serif;
-    text-shadow: 3px 5px 3px rgb(209, 207, 207);
-
-}
-
-#home h1:hover {
-    transform: scale(1.05);
-}
-
-#home p {
-    color: azure;
-    text-align: center;
-    font-size: 1.5rem;
-    font-family: 'Bree Serif', serif;
-}
-
-
-
-
-
-
-
-/* utility class */
-
-.head {
-    font-size: 4.0rem;
-    padding: 12px;
-    margin-top: 10%;
-
-}
-
-.secondary {
-    font-size: 2.8rem;
-    padding: 12px;
-
-}
-
-
-.a {
-    padding: 6px 25px;
-    color: white;
+.img {
+    margin: 20px;
     border: 2px solid white;
-    background-color: transparent;
-    margin: 7%;
-    font-size: 1.5rem;
-    border-radius: 10px;
-    font-family: 'Bree Serif', serif;
-    cursor: pointer;
-    text-decoration: none;
 }
 
-.a:hover {
-    transform: scale(1.2);
+.img img {
+    width: 200px;
+    /* Adjust as needed */
+    height: 150px;
+    /* Adjust as needed */
 }
 
-/* Footer */
-footer {
-    background-color: #aa1e48;
-    color: #fff;
+.carousel-container {
     margin-top: 10%;
-    padding: 20px;
-    text-align: center;
-    opacity: 0.6;
-    /* z-index: ; */
-    /* border: 2px solid green; */
-
+    margin-left: 20%;
+    width: 250px;
+    height: 150px;
+    perspective: 800px;
 }
 
-.foot {
-    z-index: -1;
-    color: white;
+.carousel {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    transform-style: preserve-3d;
+    animation: rotateCarousel 10s infinite linear;
 }
 
-/* .row {
-        width: 100%;
-        background-color: #aa1e48;
-    } */
+.carousel img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    transform-origin: center;
+    transition: transform 0.5s;
+    border-radius: 5%;
+}
 
-/* Mobile-first responsive design */
-@media screen and (max-width: 768px) {
-    #logo {
-        margin: 10px;
+.carousel img :hover {
+    transform: scale(1.2);
+    box-shadow: 0 0 10px black;
+}
+
+.carousel img:nth-child(1) {
+    transform: rotateY(0deg) translateZ(280px);
+}
+
+.carousel img:nth-child(2) {
+    transform: rotateY(60deg) translateZ(280px);
+}
+
+.carousel img:nth-child(3) {
+    transform: rotateY(120deg) translateZ(280px);
+}
+
+.carousel img:nth-child(4) {
+    transform: rotateY(180deg) translateZ(280px);
+}
+
+.carousel img:nth-child(5) {
+    transform: rotateY(240deg) translateZ(280px);
+}
+
+.carousel img:nth-child(6) {
+    transform: rotateY(300deg) translateZ(280px);
+}
+
+
+@keyframes rotateCarousel {
+    0% {
+        transform: rotateY(0deg);
     }
 
-    .Akki {
-        font-size: 1.5rem;
-        margin: 5px;
+    100% {
+        transform: rotateY(360deg);
     }
+}
 
-    #navbar {
-        flex-direction: column;
-        align-items: flex-start;
-        position: relative;
-    }
-
-    #navbar::before {
-        opacity: 0.6;
-    }
-
-    #navbar ul {
-        flex-direction: column;
-        align-items: center;
-        background-color: rgba(0, 0, 0, 0.8);
-        position: absolute;
-        top: var(--navbar-height);
-        width: 100%;
-        left: 0;
-        display: none;
-    }
-
-    #navbar.active ul {
-        display: flex;
-    }
-
-    #navbar ul li {
-        padding: 10px;
-        width: 100%;
-        text-align: center;
-    }
-
-    #home {
-        padding: 10px;
-        height: auto;
-    }
-
-    #home::before {
-        opacity: 0.5;
-    }
-
-    #home h1 {
-        font-size: 2.5rem;
-    }
-
-    #home p {
-        font-size: 1rem;
-    }
-
-    .head {
-        font-size: 2rem;
-        padding: 8px;
-    }
-
-    .a {
-        font-size: 1rem;
-        margin: 10px;
-        padding: 5px 15px;
-    }
-
-    footer {
-        margin-top: 20%;
-        padding: 10px;
-    }
+.whole {
+    display: flex;
 }
 </style>
-    
+  
